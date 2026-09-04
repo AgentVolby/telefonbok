9745fdd3425bfc71a232967ddb9140a1d373a691
AgentVolby <oskarzaiska@gmail.com>
2026-09-04 14:30:09 +0200

    init

---
+++ telefonbok.py
@@ -0,0 +1,11 @@
+telefonbok = []
+person1 = {
+    "navn": "Oskar",
+    "nummer": "20349567"
+}
+person2 = {
+    "navn": "Peter",
+    "nummer": "54483920"
+}
+telefonbok.append(person1)
+telefonbok.append(person2)
